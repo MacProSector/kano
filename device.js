2a12a55ac6f1c69716e63f1e87867e638bb6af06
Simon Yu <jundayu2@illinois.edu>
2020-05-30 03:44:05 -0500

    Returning variables in device scripts.

--- device.js
+++ device.js
@@ -6,9 +6,6 @@ const MotionSensorKit = require("./communitysdk/motionsensorkit");
 
 function discover(devices, print_info = false)
 {
-	console.log("");
-	console.log(`Found ${devices.length} device(s) in total.`);
-
     let devices_pixel = devices.filter((device) => {
         return device instanceof PixelKit;
     });
@@ -17,45 +14,72 @@ function discover(devices, print_info = false)
 		return device instanceof MotionSensorKit;
 	});
 
-    console.log(`Found ${devices_pixel.length} pixel device(s).`);
-	console.log(`Found ${devices_motion.length} motion device(s).`);
+	if (print_info)
+	{
+		console.log("");
+		console.log(`Found ${devices.length} device(s) in total.`);
+		console.log(`Found ${devices_pixel.length} pixel device(s).`);
+		console.log(`Found ${devices_motion.length} motion device(s).`);
+	}
+
+	return [devices_pixel, devices_motion];
 }
 
-function status_pixel(devices, print_info = false)
+function status_pixel(devices_pixel, print_info = false)
 {
-    let devices_pixel = devices.filter((device) => {
-        return device instanceof PixelKit;
-    });
+	let devices_status = []
 
     if (devices_pixel.length > 0)
 	{
 		for (i = 0; i < devices_pixel.length; i ++)
 		{
-		    devices_pixel[i].getBatteryStatus().then((status_battery) => {
-				console.log("");
-				console.log(`Pixel device ${i} battery status:`);
-				console.log(status_battery);
-			});
+		    let status_battery = devices_pixel[i].getBatteryStatus()
+				.then((status_battery) => {
+					if (print_info)
+					{
+						console.log("");
+						console.log(`Pixel device ${i} battery status:`);
+						console.log(status_battery);
+					}
 
-			devices_pixel[i].getWifiStatus().then((status_wifi) => {
-				console.log("");
-				console.log(`Pixel device ${i} Wi-Fi status:`);
-				console.log(status_wifi);
-			});
+					return status_battery;
+				});
 
-			devices_pixel[i].scanWifi().then((status_wifi_scan) => {
-				console.log("");
-				console.log(`Pixel device ${i} Wi-Fi scan status:`);
-				console.log(status_wifi_scan);
-			});	
+			let status_wifi = devices_pixel[i].getWifiStatus()
+				.then((status_wifi) => {
+					if (print_info)
+					{
+						console.log("");
+						console.log(`Pixel device ${i} Wi-Fi status:`);
+						console.log(status_wifi);
+					}
+
+					return status_wifi;
+				});
+
+			let status_wifi_scan = devices_pixel[i].scanWifi()
+				.then((status_wifi_scan) => {
+					if (print_info)
+					{
+						console.log("");
+						console.log(`Pixel device ${i} Wi-Fi scan status:`);
+						console.log(status_wifi_scan);
+					}
+
+					return status_wifi_scan;
+				});
+
+			devices_status.push([status_battery, status_wifi, status_wifi_scan]);
 		}
     }
+
+	return devices_status;
 }
 
 function main(devices)
 {
-	discover(devices, true);
-	status_pixel(devices, true);
+	const [devices_pixel, devices_motion] = discover(devices, true);
+	const devices_status = status_pixel(devices_pixel, true);
 }
 
 DeviceManager.listConnectedDevices().then((devices) => main(devices));
