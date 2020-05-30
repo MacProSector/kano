99e8fd7ee319fb51757ca773ad154b6d4d8044c2
Simon Yu <jundayu2@illinois.edu>
2020-05-30 01:55:13 -0500

    Added SDK and setup script.

--- communitysdk/__init__.py
+++
@@ -1,9 +0,0 @@
-from .rpcclient import RPCClient
-from .serialdevice import SerialDevice
-from .devicemanager import list_connected_devices
-from .motionsensorkit import MotionSensorKit
-from .retailpixelkit import RetailPixelKitSerial
-
-
-__all__ = ['RPCClient', 'SerialDevice', 'MotionSensorKit',
-    'RetailPixelKitSerial', 'list_connected_devices']
